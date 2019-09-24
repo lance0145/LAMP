c0877b380aa805466c9e096f2c48a8dba3ff5a00
Allan0145 <allan.abendanio@gwumkt.com>
2019-09-24 13:23:56 +0800

    Convert to use Heroku PostgreSQL database

--- config/database.php
+++ config/database.php
@@ -14,7 +14,7 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-	$DATABASE_URL=parse_url['postgres://ismkpakrglfaap:f9f8fe98dc4029af508e3c8f9e5f960e20639f685279803176d2d32c6acad2f6@ec2-174-129-231-116.compute-1.amazonaws.com:5432/d9q01lnanvvie3'];
+	$DATABASE_URL=parse_url('postgres://ismkpakrglfaap:f9f8fe98dc4029af508e3c8f9e5f960e20639f685279803176d2d32c6acad2f6@ec2-174-129-231-116.compute-1.amazonaws.com:5432/d9q01lnanvvie3');
 
     'default' => env('DB_CONNECTION', 'pgsql'),
 
